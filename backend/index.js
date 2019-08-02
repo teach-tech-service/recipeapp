9ed55d4bf635dbe825824dcbf9830a46a16238d9
codemasternode <marcinwarzybok@outlook.com>
2019-08-02 14:26:15 +0200

    add same origin policy

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongodbConnection from "./config/db";
 import recipeRoutes from "./routes/recipe";
 import searchRoutes from "./routes/search";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5000,
   MONGO_DB_URL =
@@ -10,11 +11,9 @@ const PORT = process.env.PORT || 5000,
 const Router = express.Router();
 
 mongodbConnection(MONGO_DB_URL);
-
+app.use(cors());
 app.use("/api/recipe", recipeRoutes(Router));
 app.use("/api/search", searchRoutes(Router));
-
-
 
 app.listen(PORT, () => {
   console.log(`Application is running on port ${PORT}`);
