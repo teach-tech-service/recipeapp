6a783dba3ac8fd50855936767910c31c993ccb97
codemasternode <marcinwarzybok@outlook.com>
2019-08-13 12:32:16 +0200

    ready to launch

---
+++ client/build/precache-manifest.5802ae37dcc0425a3a8211cf3687036f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e0d7ac3da208864b1bf64c21ddf19a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dba9b38413e5797bb01f",
+    "url": "/static/css/main.526a6686.chunk.css"
+  },
+  {
+    "revision": "cd9c901626731f8db577",
+    "url": "/static/js/2.b74cb5b9.chunk.js"
+  },
+  {
+    "revision": "dba9b38413e5797bb01f",
+    "url": "/static/js/main.96eec2ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ff06a5bbf3a6c70b67d1819e1ee90b6f",
+    "url": "/static/media/add.ff06a5bb.svg"
+  },
+  {
+    "revision": "e338802715b580908c927f7b81e3d4b7",
+    "url": "/static/media/recipeImage.e3388027.jpg"
+  },
+  {
+    "revision": "aa59497a6a3e9240c3f48a9fe7860173",
+    "url": "/static/media/time-icon.aa59497a.png"
+  }
+]);
