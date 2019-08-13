effbfb3e7a14ec35757856605b6fe398906f8165
codemasternode <marcinwarzybok@outlook.com>
2019-08-13 12:45:40 +0200

    add dotenv

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,12 @@ import mongodbConnection from "./config/db";
 import recipeRoutes from "./routes/recipe";
 import searchRoutes from "./routes/search";
 import statisticsRoutes from "./routes/statistics";
+import dotenv from "dotenv/config";
 import cors from "cors";
 
 const PORT = process.env.PORT || 5000,
   MONGO_DB_URL =
-    process.env.MONGO_DB_URL || "mongodb://localhost:27017/recipeapp",
+    process.env.MONGOLAB_URI || "mongodb://localhost:27017/recipeapp",
   app = express();
 
 mongodbConnection(MONGO_DB_URL);
